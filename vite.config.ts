bb000ec413f5e03a797ca9053223f18683396a0e
Ihor Kozak <ihorkozak3@gmail.com>
2026-03-03 15:11:10 +0200

    build: configure global access to SCSS mixins

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    },
+  },
   css: {
     modules: {
       generateScopedName: '[name]_[local]_[hash:base64:5]',
+    },
+    preprocessorOptions: {
+      scss: {
+        // Automatically injects SCSS mixins globally into all SCSS files
+        additionalData: `@use '@styles/mixins' as *;`,
+      },
     },
   },
 });
